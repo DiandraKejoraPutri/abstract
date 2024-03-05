0fa36a6e6fbf8e86ff9db83518b33b59601b17f2
DiandraKejoraPutri <156975662+DiandraKejoraPutri@users.noreply.github.com>
2024-03-05 17:13:10 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,14 @@
+package abstrak;
+
+public abstract class Shape {
+    String color;
+    
+    void setColor(String color){
+        this.color = color;
+    }
+    
+    String getColor(){
+        return this.color;
+    }
+    abstract float getArea();
+}
